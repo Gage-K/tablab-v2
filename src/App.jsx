c8731490290120dc897b42c47a10b63915a9b979
Gage-K <krausegage@gmail.com>
2025-02-17 22:48:03 -0500

    Add tab details

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 import { useState } from "react";
+import { nanoid } from "nanoid";
 
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <h1>Just testing</h1>
-    </>
+  const tabDetails = {
+    song: "Leave Them All Behind",
+    artist: "Ride",
+    creator: "Gage",
+    dateCreated: 20250214,
+    dateModified: 20250215,
+    tuning: "EADGBE",
+  };
+
+  const tab = [];
+
+  const renderDetails = (
+    <section>
+      <h1>Song: {tabDetails.song}</h1>
+      <p>Artist: {tabDetails.artist}</p>
+      <p>Creator: {tabDetails.creator}</p>
+      <p>Date created: {tabDetails.dateCreated}</p>
+      <p>Date modified: {tabDetails.dateModified}</p>
+      <p>Tuning: {tabDetails.tuning}</p>
+    </section>
   );
+
+  return <>{renderDetails}</>;
 }
 
 export default App;
