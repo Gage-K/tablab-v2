67116f36e718f82dbc712b879575f36d5dfb394d
Gage-K <krausegage@gmail.com>
2025-02-19 09:18:45 -0500

    Update display for measure handling

--- src/components/TabDisplay.jsx
+++ src/components/TabDisplay.jsx
@@ -8,33 +8,37 @@ export default function TabDisplay({
   addNewTab,
 }) {
   return (
-    // maps over all of the tab and for each moment of tab:
+    // maps over all of the tab and for each frane of tab:
     //      add an onClick that updates position
-    //      if position = index of moment, add current class
-    //      map over the array of notes in that moment
+    //      if position = index of frame, add current class
+    //      map over the array of notes in that frame
     //          display the fretted positions in notes array
 
     <section className="tab-display">
       <button className="button-plus" onClick={() => addNewTab(-1, true)}>
         +
       </button>
-      {tab.map((moment, index) => (
-        <div key={nanoid()} className="tab-moment-container">
-          <div
-            key={moment.id}
-            className={
-              index === position ? "tab-current-pos tab-moment" : "tab-moment"
-            }
-            onClick={() => updatePosition(index)}>
-            {moment.notes.map((note) => (
-              <p key={moment.notes.indexOf(note)}>{note.fret}</p>
-            ))}
-          </div>
-          <button
-            className="button-plus"
-            onClick={() => addNewTab(index, true)}>
-            +
-          </button>
+      <span>|</span>
+      {tab.map((measure, measureIndex) => (
+        <div key={measureIndex} className="tab-measure-container">
+          {measure.map((frame, frameIndex) => (
+            <div key={frameIndex} className="tab-frame-container">
+              <div
+                key={frame.id}
+                className={
+                  position.measure === measureIndex &&
+                  position.index === frameIndex
+                    ? "tab-frame tab-current-pos"
+                    : "tab-frame"
+                }
+                onClick={() => updatePosition(measureIndex, frameIndex)}>
+                {frame.notes.map((note) => (
+                  <p key={frame.notes.indexOf(note)}>{note.fret}</p>
+                ))}
+              </div>
+            </div>
+          ))}
+          <span>|</span>
         </div>
       ))}
     </section>
@@ -47,3 +51,25 @@ TabDisplay.propTypes = {
   updatePosition: PropTypes.func.isRequired,
   addNewTab: PropTypes.func.isRequired,
 };
+
+/*
+{tab.map((frame, index) => (
+        <div key={nanoid()} className="tab-frame-container">
+          <div
+            key={frame.id}
+            className={
+              index === position ? "tab-current-pos tab-frame" : "tab-frame"
+            }
+            onClick={() => updatePosition(index)}>
+            {frame.notes.map((note) => (
+              <p key={frame.notes.indexOf(note)}>{note.fret}</p>
+            ))}
+          </div>
+          <button
+            className="button-plus"
+            onClick={() => addNewTab(index, true)}>
+            +
+          </button>
+        </div>
+      ))}
+*/
