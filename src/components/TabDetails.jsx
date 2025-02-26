f682acfb64f563c8bd71f88f2e27457614c23f59
Gage-K <krausegage@gmail.com>
2025-02-26 17:11:54 -0500

    Implement interactivity and state management

--- src/components/TabDetails.jsx
+++ src/components/TabDetails.jsx
@@ -1,41 +1,95 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+
+import { TabContext } from "../App";
 
 import PropTypes from "prop-types";
 
-export default function TabDetails({
-  song,
-  artist,
-  creator,
-  dateCreated,
-  dateModified,
-  tuning,
-}) {
-  const [isShown, setIsShown] = useState(false);
+import { CaretCircleDown, CaretCircleUp } from "@phosphor-icons/react";
+
+export default function TabDetails() {
+  const notes = [
+    "A",
+    "Bb",
+    "B",
+    "C",
+    "C#",
+    "D",
+    "D#",
+    "E",
+    "F",
+    "F#",
+    "G",
+    "G#",
+  ];
+  const strings = [1, 2, 3, 4, 5, 6];
+
+  const [isShown, setIsShown] = useState(true);
+
+  const { details, setDetails } = useContext(TabContext);
+
+  console.log("in details");
+  console.log(details);
 
   return (
-    <section>
-      <h1>Song: {song}</h1>
-      <button onClick={() => setIsShown((prev) => !prev)}>
-        {isShown ? "Hide" : "Show"} details
-      </button>
+    <section className="tab-details tab-header">
+      <div className="tab-details-header">
+        <h1>Song: {details.song}</h1>
+        <button onClick={() => setIsShown((prev) => !prev)}>
+          {isShown ? <CaretCircleUp /> : <CaretCircleDown />}
+        </button>
+      </div>
       {isShown ? (
-        <>
-          <p>Artist: {artist}</p>
-          <p>Creator: {creator}</p>
-          <p>Date created: {dateCreated}</p>
-          <p>Date modified: {dateModified}</p>
-          <p>Tuning: {tuning}</p>
-        </>
+        <form>
+          <label htmlFor="song">Song name</label>
+          <input
+            name="song"
+            type="text"
+            value={details.song}
+            onChange={(event) =>
+              setDetails((prev) => ({ ...prev, song: event.target.value }))
+            }
+          />
+
+          <label htmlFor="artist">Artist</label>
+          <input
+            name="artist"
+            type="text"
+            value={details.artist}
+            onChange={(event) =>
+              setDetails((prev) => ({ ...prev, artist: event.target.value }))
+            }></input>
+
+          <fieldset>
+            <legend>Tuning</legend>
+            {strings.map((string) => (
+              <label key={string}>
+                {`String ${string}`}
+                <select
+                  value={details.tuning[string - 1]}
+                  onChange={(event) =>
+                    setDetails((prev) => ({
+                      ...prev,
+                      tuning: prev.tuning.map((note, index) =>
+                        index === string - 1 ? event.target.value : note
+                      ),
+                    }))
+                  }>
+                  {notes.map((note) => (
+                    <option value={note} key={note}>
+                      {note}
+                    </option>
+                  ))}
+                </select>
+              </label>
+            ))}
+          </fieldset>
+        </form>
       ) : null}
     </section>
   );
 }
 
 TabDetails.propTypes = {
-  song: PropTypes.string.isRequired,
-  artist: PropTypes.string.isRequired,
-  creator: PropTypes.string.isRequired,
-  dateCreated: PropTypes.number.isRequired,
-  dateModified: PropTypes.number.isRequired,
-  tuning: PropTypes.string.isRequired,
+  details: PropTypes.object.isRequired,
+  setDetials: PropTypes.func.isRequired,
 };
