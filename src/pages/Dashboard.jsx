be7296d72236d8d9e96a6372ddff9e95e75d172c
Gage-K <krausegage@gmail.com>
2025-02-27 18:20:21 -0500

    Implement create and deletion of tabs

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,20 +3,23 @@ import { Fragment, useContext } from "react";
 import { Link } from "react-router";
 
 export default function Dashboard() {
-  const { tabs } = useContext(TablabContext);
+  const { tabs, createNewTab, deleteTab } = useContext(TablabContext);
   console.log(tabs);
   return (
     <div>
+      <button onClick={createNewTab}>Create New Tab</button>
+
       {tabs.map((tab) => (
         <Fragment key={tab.details.id}>
           <Link to={`/editor/${tab.id}`}>
             <div className="db-list-item">
-              <p>{tab.id}</p>
               <p>{tab.details.song}</p>
               <p>{tab.details.artist}</p>
               <p>{tab.details.tuning.toReversed()}</p>
+              <p>{tab.id}</p>
             </div>
           </Link>
+          <button onClick={() => deleteTab(tab.id)}>Delete</button>
         </Fragment>
       ))}
     </div>
