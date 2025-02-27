ee0599a62d2e619b4ea224651fd6150012f74b36
Gage-K <krausegage@gmail.com>
2025-02-27 17:06:15 -0500

    Revert component back to initialization

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-function TabLab() {
-  return <App />;
-}
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <TabLab />
+    <App />
   </StrictMode>
 );
