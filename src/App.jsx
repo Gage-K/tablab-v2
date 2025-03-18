e491ec43d8a1243f4d216737ff9df6d727b68b0d
Gage-K <krausegage@gmail.com>
2025-03-18 12:17:05 -0400

    Add Updates and Guide routes

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,17 @@ import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
 import TablabContextLayout from "./layouts/TablabContextLayout";
 import NotFound from "./pages/NotFound";
+import Updates from "./pages/Updates";
+import Guide from "./pages/Guide";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/updates" element={<Updates />} />
+        <Route path="/guide" element={<Guide />} />
+
         <Route element={<TablabContextLayout />}>
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/editor/:tabId" element={<MainTabEditor />} />
@@ -23,6 +28,7 @@ function App() {
             element={<Navigate to="/notfound" replace />}
           />
         </Route>
+
         <Route path="notfound" element={<NotFound />} />
         <Route path="*" element={<Navigate to="/notfound" replace />} />
       </Routes>
