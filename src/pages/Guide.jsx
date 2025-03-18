7c6dc7a3beacd376b3e09492ec3ebc2c2b576bac
Gage-K <krausegage@gmail.com>
2025-03-18 12:17:34 -0400

    init commit

---
+++ src/pages/Guide.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+import PageWrapper from "../layouts/PageWrapper";
+
+export default function Guide() {
+  return (
+    <>
+      <Header />
+      <PageWrapper>
+        <h1 className="text-5xl font-bold text-neutral-800 pt-8 mb-10">
+          Guide
+        </h1>
+      </PageWrapper>
+    </>
+  );
+}
