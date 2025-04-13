e7e207628bbcf29b68614cbddd208b309f486e33
Gage-K <krausegage@gmail.com>
2025-04-13 17:49:19 -0400

    Add routes for fetching tab data

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,27 @@
 const { Router } = require("express");
 const router = Router();
+const db = require("../db/queries");
+const indexController = require("../controllers/index");
+const usersController = require("../controllers/usersController");
 
-router.get("/", (req, res) => res.render("index"));
+router.get("/", indexController.getUserData);
 router.get("/login", (req, res) => res.render("login"));
-router.get("/signup", (req, res) => res.render("signup"));
+router.get("/register", (req, res) => res.render("signup"));
+router.get("/test", (req, res) => res.render("test"));
+router.get("/tabs", async (req, res) => {
+  try {
+    const user = res.locals.currentUser;
+    if (!user) {
+      res.redirect("/login");
+    } else {
+      const tabs = await db.getTabsByUser(user.id);
+      console.log(tabs);
+      res.json(tabs);
+      res.status(201);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 module.exports = router;
