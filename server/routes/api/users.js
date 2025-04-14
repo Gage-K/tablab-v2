6f5b661272a8b9b742b3a60b4e6e1c72377b9e93
Gage-K <krausegage@gmail.com>
2025-04-14 15:10:33 -0400

    Remove unnecessary params to fit JWT flow

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -4,22 +4,22 @@ const usersController = require("../../controllers/usersController");
 const passport = require("passport");
 
 router.get(
-  "/:id",
+  "/",
   passport.authenticate("jwt", { session: false }),
   usersController.getUser
 ); // need session false to ignore the session middleware
 router.put(
-  "/:id",
+  "/",
   passport.authenticate("jwt", { session: false }),
   usersController.updateUserEmail
 );
 router.post(
-  "/:id",
+  "/",
   passport.authenticate("jwt", { session: false }),
   usersController.insertUser
 );
 router.delete(
-  "/:id",
+  "/",
   passport.authenticate("jwt", { session: false }),
   usersController.deleteUser
 );
