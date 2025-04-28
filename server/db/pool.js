61369acc7975a85932bc03aa1d2bdeb36567f239
Gage-K <krausegage@gmail.com>
2025-04-28 08:59:40 -0400

    Update URL

--- server/db/pool.js
+++ server/db/pool.js
@@ -6,7 +6,6 @@ module.exports =
     ? new Pool({
         connectionString: process.env.DB_URL,
         ssl: { rejectUnauthorized: false },
-        host: new URL(process.env.DB_URL).hostname,
       })
     : new Pool({
         host: process.env.DB_HOST,
