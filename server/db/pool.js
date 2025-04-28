89e00868d4a3a5b85f716474576e8051af767d85
Gage-K <krausegage@gmail.com>
2025-04-27 22:49:01 -0400

    Config pool for production db URL

--- server/db/pool.js
+++ server/db/pool.js
@@ -1,10 +1,14 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-module.exports = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+const connectionURL = process.env.DB_URL;
+
+module.exports = connectionURL
+  ? new Pool({ connectionURL, ssl: { rejectUnauthorized: false } })
+  : new Pool({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT,
+    });
