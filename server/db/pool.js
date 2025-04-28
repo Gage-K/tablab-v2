1554cecc11445479a23a89b38047e62e5b43d7d6
Gage-K <krausegage@gmail.com>
2025-04-28 08:51:18 -0400

    Force IPv4

--- server/db/pool.js
+++ server/db/pool.js
@@ -6,6 +6,7 @@ module.exports =
     ? new Pool({
         connectionString: process.env.DB_URL,
         ssl: { rejectUnauthorized: false },
+        host: new URL(connectionURL).hostname,
       })
     : new Pool({
         host: process.env.DB_HOST,
