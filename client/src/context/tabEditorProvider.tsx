db8454053b3d936207d81ba807d05aea8f67925c
Gage-K <krausegage@gmail.com>
2026-03-22 23:59:43 -0400

    fix dispatch bug for clearing cursor selection

--- client/src/context/tabEditorProvider.tsx
+++ client/src/context/tabEditorProvider.tsx
@@ -291,6 +291,15 @@ export function TabEditorProvider({
               fret: FRET_EMPTY,
             },
           });
+          dispatch({
+            type: "SET_NOTE_STYLE",
+            payload: {
+              measure: position.measure,
+              frame: position.frame,
+              string: position.string,
+              style: "none",
+            },
+          });
           break;
         case "muteNote":
           dispatch({
